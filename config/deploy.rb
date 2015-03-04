96d5b0d16b70270f784139d1a9a75730ff775f5b
Gino Clement <ginoclement@gmail.com>
2015-03-04 02:44:41 -0800

    Removing config/application.yml from linked files since it doens't exist

--- config/deploy.rb
+++ config/deploy.rb
@@ -12,7 +12,7 @@ set :keep_releases, 5
 # set :pty, true
 
 # Files we want symlinking to specific entries in shared.
-set :linked_files, %w{config/database.yml config/application.yml config/secrets.yml}
+set :linked_files, %w{config/database.yml config/secrets.yml}
 
 # Default value for linked_dirs is []
 # set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')

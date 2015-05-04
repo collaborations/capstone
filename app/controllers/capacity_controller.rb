ac20b18c5358dfdad98b819b64fbd337a024dec1
Gino Clement <ginoclement@gmail.com>
2015-05-04 14:45:36 -0700

    Skipping verify authenticity token. Will need to find another work around as this is likely allowing for CSRF attacks. At this point priority is a prototype.

--- app/controllers/capacity_controller.rb
+++ app/controllers/capacity_controller.rb
@@ -1,10 +1,22 @@
 class CapacityController < ApplicationController
+  # Introducing this has to give some sort of CSRF vulnerability
+  skip_before_action :verify_authenticity_token
 
   def index
   end
 
   def get
-    render text: "Thanks for sending a POST request with cURL!"
+    id = 1
+    # Should get total from the institution as the maximum number of spots allowed
+    total = 50
+    # Will want to change this to return the data for the current date.
+    data = Capacity.where(id: id).first
+    render json: {
+        "reserved": data.reserved,
+        "reserved_confirmed": data.reserved_confirmed,
+        "standby": data.standby,
+        "total": total
+      }
   end
 
   def update

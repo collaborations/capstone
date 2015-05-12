a0fbff1aee713ef23ca5d27233b688920b22431c
Gino Clement <ginoclement@gmail.com>
2015-05-12 08:16:41 -0700

    Started adding logic in order to send an SMS with Twilio.

--- app/controllers/sms_controller.rb
+++ app/controllers/sms_controller.rb
@@ -4,9 +4,31 @@ class SmsController < ApplicationController
     render text: "Implement Sending SMS"
   end
   
-  def subscribe
+  def retrieve_messages(params)
   end
 
-  def unsubscribe
+  def send_message(params)
+    message = params.require(:message)
+    numbers = params.require(:numbers)
+
+    numbers.each do |num|
+      begin
+        @client = Twilio::REST::Client.new Settings.twilio.sid, Settings.twilio.auth
+        @client.account.messages.create({
+          :from => Settings.twilio.number,
+          :to => num,
+          :body => message
+        })
+      rescue Twilio::REST::RequestError => e
+        puts e.message
+      end
+    end
   end
+
+  def subscribe(number, institution_id)
+  end
+
+  def unsubscribe(number, institution_id)
+  end
+
 end

0e3fd4aac9668312354ae827a187ad60bddb1043
Gino Clement <ginoclement@gmail.com>
2015-05-12 20:02:51 -0700

    A bunch of random stuff?

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150511215245) do
+ActiveRecord::Schema.define(version: 20150513024437) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -85,6 +85,13 @@ ActiveRecord::Schema.define(version: 20150511215245) do
     t.datetime "created_at",     null: false
     t.datetime "updated_at",     null: false
     t.integer  "institution_id"
+  end
+
+  create_table "subscribers", force: :cascade do |t|
+    t.string   "phone",          null: false
+    t.integer  "institution_id", null: false
+    t.datetime "created_at",     null: false
+    t.datetime "updated_at",     null: false
   end
 
   create_table "users", force: :cascade do |t|

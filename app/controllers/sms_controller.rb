723093b32143329d8628daa4d78ff09a377ad465
Gino Clement <ginoclement@gmail.com>
2015-05-16 15:11:32 -0700

    Got a basic info text going out. I need to do some error handling and we need to decide on what fields get sent, but it works.

--- app/controllers/sms_controller.rb
+++ app/controllers/sms_controller.rb
@@ -17,9 +17,68 @@ class SmsController < ApplicationController
   end
 
   def info
-    puts params
+#     Parameters: {
+          # "message_type"=>"text",
+          # "institution_id"=>"4",
+          # "address"=>"false",       - Locations
+          # "phone"=>"false",         - Contact
+          # "amenities"=>"false",     - InstitutionHasAmenities
+          # "restrictions"=>"false",  - Restrictions
+          # "hours"=>"false",         - InstitutionDetails
+          # "email"=>"",
+          # "sms"=>"2063713424"
+    begin
+      id = params.require(:institution_id)
+      number = params.require(:sms)
+      institution = Institution.where(id: params[:id]).first
+      message = []
+
+      if params[:phone].to_bool
+        contact = Contact.where(institution_id: id)
+        if contact.present? and contact.phone.present?
+          phone = contact.phone.split("-")
+          message << sprintf("Phone: (%s)%s-%s", phone[0], phone[1], phone[2])
+        end
+      end
+
+      if params[:hours].to_bool
+        # details = InstitutionDetails.where(institution_id: id)
+        # details = details.first if details.present? and details.hours.present?
+        message << "Hours: MF"
+      end
+
+      if params[:address].to_bool
+        address = Location.where(institution_id: id)
+        address = address.first if address.present?
+        message << "Address:"
+        message << address.streetLine1
+        message << address.streetLine2 if address.streetLine2.present?
+        message << sprintf("%s, %s %s", address.city, address.state, address.zip)
+      end
+
+      if params[:amenities].to_bool
+        amenities = InstitutionHasAmenities.where(institution_id: id)
+        message << "Amenity".pluralize(amenities.size) if amenities.present?
+        amenities.each do |a|
+          message << a.name.capitalize
+        end
+      end
+
+      if params[:restrictions].to_bool
+        restrictions = Restrictions.where(institution_id: id)
+        message << "Restriction".pluralize(restrictions.size) if restrictions.present?
+        restrictions.each do |r|
+          message << r.name.capitalize
+        end
+      end
+
+      body = message.join("\n")
+      send_message([number], body)
+    rescue ActionController::ParameterMissing => e
+      puts e.message
+    end
     head :ok, content_type: "text/html"
-    # send_message([number], body)
+    
   end
 
   # POST /institution/subscribe

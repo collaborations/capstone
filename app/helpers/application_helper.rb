4e6bae3eb8f778f00a453ac9fc3bb006c91aabcc
Nikki Nikkhoui <nnikkhoui@gmail.com>
2015-05-30 20:04:17 -0700

    Changing amenity helper method so that it doesn't use instance variables.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,16 +1,8 @@
 module ApplicationHelper
 
-  def breadcrumbs
-    @url = request.env['PATH_INFO'].sub(/\A\//, '').split("/")
-    if @url.size >= 1
-      render 'shared/breadcrumbs'
-    end
-  end
-
   def embedded_amenity(amenity, options = {})
-    @amenity = amenity
     @label = true if options[:label].present?
-    render 'shared/amenity'
+    render 'shared/amenity', amenity: amenity 
   end
 
   def embedded_svg(filename, options = {})

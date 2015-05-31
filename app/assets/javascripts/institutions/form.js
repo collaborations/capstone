c47a749ea59fd04d4d0db1ace3a99e687e575642
Gino Clement <ginoclement@gmail.com>
2015-05-31 07:50:57 -0700

    There's no reason to keep this file, I started creating it when I was planning to implement hours a different way and forgot to remove it.

--- app/assets/javascripts/institutions/form.js
+++
@@ -1,9 +0,0 @@
-if($("#institutions_edit").length || $("#institutions_new").length ){
-  window.onload = initialize;
-}
-
-function initialize(){
-  $("#add-hours").on('click', function(){
-    alert("Add another field");
-  })
-}

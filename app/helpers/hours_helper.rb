95899c3f4e6d21e4c9b136fde2dd8ce6d8a3a830
Gino Clement <ginoclement@gmail.com>
2015-06-01 15:49:35 -0700

    Changed the way hours are processed. I believe businesstime is being used correctly now.

--- app/helpers/hours_helper.rb
+++ app/helpers/hours_helper.rb
@@ -13,11 +13,11 @@ module HoursHelper
     @open = false
 
     id = (institution_id.present?) ? institution_id : @institution.id
-    time_format = t('hours.time.format')
-    week_days = [:sun, :mon, :tue, :wed, :thu, :fri, :sat]
+    time_format_print = t('hours.time_format.print')
+    time_format_compare = t('hours.time_format.compare')
+    weekdays = [:sun, :mon, :tue, :wed, :thu, :fri, :sat]
 
     h = Hours.where(institution_id: id).first
-    hours = []
     temp = [ 
               [ h[:sun_open], h[:sun_close] ],
               [ h[:mon_open], h[:mon_close] ],
@@ -27,29 +27,47 @@ module HoursHelper
               [ h[:fri_open], h[:fri_close] ],
               [ h[:sat_open], h[:sat_close] ]
            ]
-    work_days = []
+
+    hours = []
+    work_days = Hash.new
+
     temp.each_with_index do |t, i|
-      open = t[0].present? ? t[0].strftime(time_format) : nil
-      close = t[1].present? ? t[1].strftime(time_format) : nil
+      open = t[0].present? ? t[0] : nil
+      close = t[1].present? ? t[1] : nil
+
       if open.present? and close.present?
         @hours_present = true
-        hours << open + "-" + close
-        work_days << week_days[i]
+        hours << open.strftime(time_format_print) + "-" + close.strftime(time_format_print)
+        work_days[weekdays[i]] = [open.strftime(time_format_compare), close.strftime(time_format_compare)]
       else
         hours << "Closed"
       end
     end
 
     # Whether the institution is current open
-    c_time = Time.now
-    h_today = temp[c_time.wday]
+    c_time = Time.now.strftime(time_format_compare)
+    # h_today = temp[c_time.wday]
 
-    if h_today[0].present? and h_today[1].present?
-      BusinessTime::Config.work_week = work_days
-      BusinessTime::Config.beginning_of_workday = h_today[0].strftime(time_format)
-      BusinessTime::Config.end_of_workday = h_today[1].strftime(time_format)
-    end
-    open = c_time.during_business_hours?
+    # if h_today[0].present? and h_today[1].present?
+    #   BusinessTime::Config.work_week = work_days
+    #   BusinessTime::Config.beginning_of_workday = h_today[0].strftime(time_format_compare)
+    #   BusinessTime::Config.end_of_workday = h_today[1].strftime(time_format_compare)
+    # end
+    
+    
+    @open = Time.parse(c_time).during_business_hours?
+
+    puts "\n"*20
+    puts "="*20
+    puts work_days.to_json
+    puts "\n"
+    puts c_time
+    puts "\n"
+    puts Time.parse(c_time)
+    puts "\n"
+    puts BusinessTime::Config.work_week
+    puts "="*20
+    puts "\n"*20
 
     if hours.present?
       render 'shared/hours', hours: hours
